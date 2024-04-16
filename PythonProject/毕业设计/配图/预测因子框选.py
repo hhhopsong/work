259aa27b7aa347035d6e6e4be3c32c0315394b63
hhhopsong <1057422494@qq.com>
2024-04-16 10:36:28 +0800

    🚀

--- PythonProject/毕业设计/配图/预测因子框选.py
+++ PythonProject/毕业设计/配图/预测因子框选.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.transforms as mtransforms
 import matplotlib.patheffects as path_effects
 import matplotlib.path as mpath
+import matplotlib.patches as mpatches
 from cnmaps import get_adm_maps, draw_maps
 from matplotlib import ticker
 import cmaps
@@ -36,27 +37,33 @@ PC_78 = eof_78.pcs(pcscaling=1, npcs=2)  # 同上 npcs决定输出的时间序�
 s_78 = eof_78.varianceFraction(neigs=2)  # 得到前neig个模态的方差贡献
 # 数据读取
 sat = xr.open_dataset(r"C:\Users\10574\OneDrive\File\Graduation Thesis\ThesisData\ERA5\ERA5_2mTemperature_MeanSlp.nc")
-
+sst = xr.open_dataset(r"C:\Users\10574\Desktop\data\sst.mnmean.nc")  # NetCDF-4文件路径不可含中文
 # 数据切片
 T2m = sat['t2m'].sel(time=slice('1979-01-01', '2014-12-31'))
 T2m_78 = T2m.sel(time=T2m.time.dt.month.isin([7, 8]))
+sst = sst['sst'].sel(time=slice('1979-01-01', '2014-12-31'))
+sst = sst.sel(time=sst.time.dt.month.isin([7, 8]))
 # 经纬度
 lon_sat = T2m['longitude']
 lat_sat = T2m['latitude']
-
+lon_sst = sst['lon']
+lat_sst = sst['lat']
 # 将七八月份数据进行每年平均
 sat_78 = T2m_78.groupby('time.year').mean('time')
 sat_78 = np.array(sat_78)
+sst_78 = sst.groupby('time.year').mean('time')
+sst_78 = np.array(sst_78)
 try:
     # 读取相关系数
     reg_sat = xr.open_dataset(r'D:\CODES\Python\PythonProject\cache\Graduation Thesis\reg_sat.nc')
+    reg_sst = xr.open_dataset(r'D:\CODES\Python\PythonProject\cache\Graduation Thesis\reg_sst.nc')
 except:
     # 将数据回归到PC上
     reg_sat = [[np.polyfit(PC_78[:, pc_index], sat_78[:, ilat, ilon], 1)[0] for ilon in range(len(lon_sat))] for ilat in tqdm(range(len(lat_sat)), desc='计算SAT', position=0, leave=True)]
     xr.DataArray(reg_sat, coords=[lat_sat, lon_sat], dims=['lat', 'lon']).to_netcdf(r'D:\CODES\Python\PythonProject\cache\Graduation Thesis\reg_sat.nc')
     ###数据再读取
     reg_sat = xr.open_dataset(r'D:\CODES\Python\PythonProject\cache\Graduation Thesis\reg_sat.nc')
-
+    reg_sst = xr.open_dataset(r'D:\CODES\Python\PythonProject\cache\Graduation Thesis\reg_sst.nc')
 # 进行显著性0.05检验
 from scipy.stats import t
 
@@ -66,6 +73,11 @@ n = len(PC_78[:, 0])
 # 计算t值
 Lxx = np.sum((PC_78[:, pc_index] - np.mean(PC_78[:, pc_index])) ** 2)
 # SST
+Sr_sst = reg_sst**2 * Lxx
+St_sst = np.sum((sst_78 - np.mean(sst_78, axis=0)) ** 2, axis=0)
+σ_sst = np.sqrt((St_sst - Sr_sst) / (n - 2))
+t_sst = reg_sst * np.sqrt(Lxx) / σ_sst
+
 Sr_sat = reg_sat**2 * Lxx
 St_sat = np.sum((sat_78 - np.mean(sat_78, axis=0)) ** 2, axis=0)
 σ_sat = np.sqrt((St_sat - Sr_sat) / (n - 2))
@@ -78,22 +90,24 @@ p_sat78 = np.zeros((len(lat_sat), len(lon_sat)))
 p_sat78.fill(np.nan)
 p_sat78[np.abs(t_sat['__xarray_dataarray_variable__'].to_numpy()) > t_critical] = 1
 
+p_sst78 = np.zeros((len(lat_sst), len(lon_sst)))
+p_sst78.fill(np.nan)
+p_sst78[np.abs(t_sst['__xarray_dataarray_variable__'].to_numpy()) > t_critical] = 1
+
 # 绘图
 # ##地图要素设置
 plt.rcParams['font.sans-serif'] = ['Arial']
 plt.rcParams['axes.unicode_minus'] = False
-plt.subplots_adjust(wspace=0.1, hspace=0.001)  # wspace、hspace左右、上下的间距
 font = {'family' : 'Arial','weight' : 'bold','size' : 12}
-# plt.subplots_adjust(wspace=0.1, hspace=0.32)  # wspace、hspace左右、上下的间距
+plt.subplots_adjust(wspace=0.3, hspace=0.32)  # wspace、hspace左右、上下的间距
 extent1 = [0, 360, 50, 90]  # 经度范围，纬度范围
-xticks1 = np.arange(extent1[0], extent1[1] + 1, 10)
-yticks1 = np.arange(extent1[2], extent1[3] + 1, 10)
+
 
 fig = plt.figure(figsize=(10, 10))
 
 # ##ax1 Corr. PC1 & JA SST,2mT
 level1 = [-1, -.7, -.4, -.1, -.05, .05, .1, .4, .7, 1]
-ax1 = fig.add_subplot(1,1,1, projection=ccrs.NorthPolarStereo(central_longitude=90))
+ax1 = fig.add_subplot(121, projection=ccrs.NorthPolarStereo(central_longitude=90))
 ax1.set_extent(extent1, crs=ccrs.PlateCarree())
 # 去除fill_value 1e+20
 # 去除180白线!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!加上截距
@@ -110,7 +124,12 @@ p_sat = np.where(p_sat == 1, 0, np.nan)
 a1_uv = ax1.quiver(a1_lon_sat, lat_sat, p_sat, p_sat, scale=20, color='black', headlength=3,
                    regrid_shape=60, headaxislength=3, transform=ccrs.PlateCarree(central_longitude=0), width=0.005)
 # 框选预测因子
-ax1.plot([70, 70, 50, 50, 70], [33, 64.4, 64.4, 33, 33], color='red', linewidth=2, linestyle='-',transform=ccrs.PlateCarree(central_longitude=90))
+lon1, lon2, lat1, lat2 = 36, 62, 54, 70
+smoonth = 100
+lon_ = [i for i in np.linspace(lon1, lon2, smoonth)] + [i for i in np.linspace(lon1, lon2, smoonth)][::-1] + [lon1]
+lat_ = [lat1 for i in range(smoonth)] + [lat2 for i in range(smoonth)] + [lat1]
+ax1.plot(lon_, lat_, color='blue', linewidth=2, linestyle='-',transform=ccrs.PlateCarree(central_longitude=0))
+
 grid_lon = ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, color='grey',linestyle='--')
 grid_lon.xlocator = FixedLocator(np.linspace(-180,180,13))
 grid_lon.ylocator = FixedLocator([65, 80])
@@ -123,38 +142,82 @@ verts = np.vstack([np.sin(theta), np.cos(theta)]).T
 circle = mpath.Path(verts * radius + center)
 ax1.set_boundary(circle, transform=ax1.transAxes)
 
-'''# 刻度线设置
-ax1.set_xticks(xticks1, crs=proj)
+# ax2 Corr. PC2 & JA PRE,850UV
+proj = ccrs.PlateCarree(central_longitude=180)
+extent2 = [-180, 0, -30, 80]
+xticks1 = np.arange(extent2[0], extent2[1] + 1, 10)
+yticks1 = np.arange(extent2[2], extent2[3] + 1, 10)
+level_sst = [-.3, -.25, -.2, -.15, -.1, -.05, .05, .1, .15, .2, .25, .3]
+radio = 0.3
+print('开始绘制地图2')
+ax2 = fig.add_subplot(122, projection=proj)
+ax2.set_extent(extent2, crs=proj)
+# 去除180白线
+reg_sst, a2_sst_lon = add_cyclic_point(reg_sst['__xarray_dataarray_variable__'].to_numpy(), coord=lon_sst)
+p_sst, a2_lon_sst = add_cyclic_point(p_sst78, coord=lon_sst)
+p_sst = np.where(p_sst == 1, 0, np.nan)
+
+ax2.set_title('(b)Reg SST', fontsize=20, loc='left')
+a2 = ax2.contourf(a2_sst_lon, lat_sst, reg_sst, cmap=cmaps.GMT_polar[4:10]+cmaps.CBR_wet[0]+cmaps.GMT_polar[10:16], levels=level_sst, extend='both',
+                  transform=ccrs.PlateCarree())
+
+a2_p = ax2.quiver(a2_lon_sst, lat_sst, p_sst, p_sst, scale=20, color='black',
+                  regrid_shape=60, headlength=2, headaxislength=2, transform = ccrs.PlateCarree())
+
+# 框选预测因子
+lon1, lon2, lat1, lat2 = 55, 100, -8, 5
+smoonth = 100
+lon_ = [i for i in np.linspace(lon1, lon2, smoonth)] + [i for i in np.linspace(lon1, lon2, smoonth)][::-1] + [lon1]
+lat_ = [lat1 for i in range(smoonth)] + [lat2 for i in range(smoonth)] + [lat1]
+ax2.plot(lon_, lat_, color='blue', linewidth=2, linestyle='-',transform=ccrs.PlateCarree())
+
+ax2.add_feature(cfeature.LAND.with_scale('110m'), color='lightgray')# 添加陆地并且陆地部分全部填充成浅灰色
+ax2.add_feature(cfeature.COASTLINE.with_scale('110m'), linewidth=.3)  # 添加海岸线
+
+
+# 刻度线设置
+ax2.set_xticks(xticks1, crs=proj)
+ax2.set_yticks(yticks1, crs=proj)
 lon_formatter = LongitudeFormatter()
 lat_formatter = LatitudeFormatter()
-ax1.xaxis.set_major_formatter(lon_formatter)
-font = {'family': 'Arial', 'weight': 'bold', 'size': 28}
+ax2.xaxis.set_major_formatter(lon_formatter)
+ax2.yaxis.set_major_formatter(lat_formatter)
+font = {'family': 'Arial', 'weight': 'bold', 'size': 16}
 
 xmajorLocator = MultipleLocator(60)  # 先定义xmajorLocator，再进行调用
-ax1.xaxis.set_major_locator(xmajorLocator)  # x轴最大刻度
+ax2.xaxis.set_major_locator(xmajorLocator)  # x轴最大刻度
 xminorLocator = MultipleLocator(10)
-ax1.xaxis.set_minor_locator(xminorLocator)  # x轴最小刻度
-
+ax2.xaxis.set_minor_locator(xminorLocator)  # x轴最小刻度
+ymajorLocator = MultipleLocator(30)
+ax2.yaxis.set_major_locator(ymajorLocator)  # y轴最大刻度
+yminorLocator = MultipleLocator(10)
+ax2.yaxis.set_minor_locator(yminorLocator)  # y轴最小刻度
 # ax1.axes.xaxis.set_ticklabels([]) ##隐藏刻度标签
 # 最大刻度、最小刻度的刻度线长短，粗细设置
-ax1.tick_params(which='major', length=11, width=2, color='darkgray')  # 最大刻度长度，宽度设置，
-ax1.tick_params(which='minor', length=8, width=1.8, color='darkgray')  # 最小刻度长度，宽度设置
-ax1.tick_params(which='both', bottom=True, top=False, left=True, labelbottom=True, labeltop=False)
+ax2.tick_params(which='major', length=11, width=2, color='darkgray')  # 最大刻度长度，宽度设置，
+ax2.tick_params(which='minor', length=8, width=1.8, color='darkgray')  # 最小刻度长度，宽度设置
+ax2.tick_params(which='both', bottom=True, top=False, left=True, labelbottom=True, labeltop=False)
 plt.rcParams['xtick.direction'] = 'out'  # 将x轴的刻度线方向设置向内或者外
 # 调整刻度值字体大小
-ax1.tick_params(axis='both', labelsize=28, colors='black')
+ax2.tick_params(axis='both', labelsize=16, colors='black')
 # 设置坐标刻度值的大小以及刻度值的字体
-labels = ax1.get_xticklabels()
+labels = ax2.get_xticklabels() + ax2.get_yticklabels()
 [label.set_fontname('Arial') for label in labels]
-font2 = {'family': 'Arial', 'weight': 'bold', 'size': 28}'''
+font2 = {'family': 'Arial', 'weight': 'bold', 'size': 16}
+
 
 # color bar位置
 # position = fig.add_axes([0.296, 0.08, 0.44, 0.011])#位置[左,下,右,上]
-position1 = fig.add_axes([0.146, 0.01, 0.74, 0.03])
+position1 = fig.add_axes([0.146, 0.2, 0.3, 0.015])
 cb1 = plt.colorbar(a1, cax=position1, orientation='horizontal')  # orientation为水平或垂直
-cb1.ax.tick_params(length=1, labelsize=14)  # length为刻度线的长度
+cb1.ax.tick_params(length=0, labelsize=14)  # length为刻度线的长度
 cb1.locator = ticker.FixedLocator([-1, -.7, -.4, -.1, 0, .1, .4, .7, 1]) # colorbar上的刻度值个数
 
+position2 = fig.add_axes([0.546, 0.2, 0.3, 0.015])
+cb2 = plt.colorbar(a1, cax=position2, orientation='horizontal')  # orientation为水平或垂直
+cb2.ax.tick_params(length=0, labelsize=14)  # length为刻度线的长度
+cb2.locator = ticker.FixedLocator([-.3, -.2, -.1, .1, .2, .3]) # colorbar上的刻度值个数
 
-plt.savefig(r'C:\Users\10574\OneDrive\File\Graduation Thesis\论文配图\2m温度观测场回归.png', dpi=1000, bbox_inches='tight')
+
+plt.savefig(r'C:\Users\10574\OneDrive\File\Graduation Thesis\论文配图\预测因子2mT.png', dpi=1000, bbox_inches='tight')
 plt.show()
