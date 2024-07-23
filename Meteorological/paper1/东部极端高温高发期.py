0a46da0959e5c44a73f2b1ed57a430759c6cd496
hhhopsong <1057422494@qq.com>
2024-07-23 21:52:49 +0800

    🚀

--- Meteorological/paper1/东部极端高温高发期.py
+++ Meteorological/paper1/东部极端高温高发期.py
@@ -18,7 +18,7 @@ from toolbar.masked import masked   # 气象工具函数
 CN051_1 = xr.open_dataset(r"E:\data\CN05.1\1961_2021\CN05.1_Tmax_1961_2021_daily_025x025.nc")
 CN051_2 = xr.open_dataset(r"E:\data\CN05.1\2022\CN05.1_Tmax_2022_daily_025x025.nc")
 Tmax_cn051 = xr.concat([CN051_1, CN051_2], dim='time')
-if eval(input("是否计算全国极端高温95分位相对阈值(0/1)?")):
+if eval(input("是否计算全国极端高温95分位相对阈值(0/1)?\n")):
     Tmax_sort95 = Tmax_cn051['tmax'].sel(time=slice('1979-01-01', '2022-12-31')).quantile(0.95, dim='time')     # 全国极端高温95分位相对阈值
     Tmax_sort95.to_netcdf(r"D:\CODES\Python\Meteorological\paper1\cache\NationalHighTemp_95threshold.nc")
     del Tmax_sort95     # 释放Tmax_sort95占用内存,优化代码性能
@@ -30,7 +30,7 @@ Tmax_sort95 = masked(Tmax_sort95, r"C:\Users\10574\OneDrive\File\气象数据资
 # ##地图要素设置
 plt.rcParams['font.sans-serif'] = ['Arial']
 plt.rcParams['axes.unicode_minus'] = False
-extent_CN = [70, 140, 15, 55]  # 中国大陆经度范围，纬度范围
+extent_CN = [88, 124, 22, 38]  # 中国大陆经度范围，纬度范围
 proj = ccrs.PlateCarree()   # 投影方式
 fig = plt.figure(figsize=(16, 9))   # 创建画布
 ax1 = fig.add_subplot(111, projection=proj)  # 添加子图
@@ -38,6 +38,7 @@ ax1.set_extent(extent_CN, crs=proj) # 设置地图范围
 a1 = ax1.contourf(Tmax_sort95['lon'], Tmax_sort95['lat'], Tmax_sort95['tmax'], cmap=cmaps.WhiteBlueGreenYellowRed, levels=15, extend='both', transform=proj)
 ax1.add_feature(cfeature.LAND.with_scale('10m'),color='lightgray')# 添加陆地并且陆地部分全部填充成浅灰色
 ax1.add_geometries(Reader(r'C:\Users\10574\OneDrive\File\气象数据资料\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp').geometries(), ccrs.PlateCarree(), facecolor='none', edgecolor='black', linewidth=0.4)
+ax1.add_geometries(Reader(r'D:\CODES\Python\Meteorological\maps\cnriver\长江\长江.shp').geometries(), ccrs.PlateCarree(), facecolor='none', edgecolor='blue', linewidth=0.2)
 plt.savefig(r'C:\Users\10574\desktop\图2.png', dpi=1500, bbox_inches='tight')
 plt.show()
 pass
---
+++ Meteorological/paper1/数据预处理及计算.py
@@ -0,0 +1,54 @@
+from cartopy import crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.io.shapereader import Reader
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter  # 专门提供经纬度的
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mtransforms
+from cnmaps import get_adm_maps, draw_maps
+from matplotlib import ticker
+import cmaps
+from matplotlib.ticker import MultipleLocator
+from toolbar.masked import masked   # 气象工具函数
+import pandas as pd
+import tqdm
+
+
+# 数据读取
+data_year = ['1979', '2022']
+# 读取CN05.1逐日最高气温数据
+CN051_1 = xr.open_dataset(r"E:\data\CN05.1\1961_2021\CN05.1_Tmax_1961_2021_daily_025x025.nc")
+CN051_2 = xr.open_dataset(r"E:\data\CN05.1\2022\CN05.1_Tmax_2022_daily_025x025.nc")
+Tmax_cn051 = xr.concat([CN051_1, CN051_2], dim='time')
+if eval(input("1)是否计算全国极端高温95分位相对阈值(0/1)?\n")):
+    Tmax_sort95 = Tmax_cn051['tmax'].sel(time=slice(data_year[0]+'-01-01', data_year[1]+'-12-31')).quantile(0.95, dim='time')     # 全国极端高温95分位相对阈值
+    Tmax_sort95.to_netcdf(r"D:\CODES\Python\Meteorological\paper1\cache\NationalHighTemp_95threshold.nc")
+    del Tmax_sort95     # 释放Tmax_sort95占用内存,优化代码性能
+Tmax_sort95 = xr.open_dataset(r"cache\NationalHighTemp_95threshold.nc")  # 读取缓存
+if eval(input("2)是否计算全国极端高温日温度距平(0/1)?\n")):
+    EHD = Tmax_cn051.sel(time=slice(data_year[0]+'-01-01', data_year[1]+'-12-31')) - Tmax_sort95  # 温度距平
+    EHD = EHD.where(EHD >= 0, np.nan)  # 极端高温日温度距平
+    EHD = EHD-EHD+1  # 数据二值化处理(1:极端高温,np.nan:非极端高温)
+    EHD.to_netcdf(r"D:\CODES\Python\Meteorological\paper1\cache\EHD.nc")
+    del EHD  # 释放EHD占用内存,优化代码性能
+EHD = xr.open_dataset(r"cache\EHD.nc")  # 读取缓存
+EHD = masked(EHD, r"C:\Users\10574\OneDrive\File\气象数据资料\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp")  # 掩膜处理得长江流域EHD温度距平
+EHD = EHD.sel(time=EHD['time.month'].isin([6, 7, 8, 9]))  # 选择6、7、8、9月数据
+# 将数据按月分组
+EHD_6 = EHD.sel(time=EHD['time.month'] == 6)
+EHD_7 = EHD.sel(time=EHD['time.month'] == 7)
+EHD_8 = EHD.sel(time=EHD['time.month'] == 8)
+EHD_9 = EHD.sel(time=EHD['time.month'] == 9)
+# 将数据按日分组,并计算每日的年平均极端高温日数
+EHD_6 = EHD_6.groupby('time.day').sum()/(eval(data_year[1])-eval(data_year[0])+1)
+EHD_7 = EHD_7.groupby('time.day').sum()/(eval(data_year[1])-eval(data_year[0])+1)
+EHD_8 = EHD_8.groupby('time.day').sum()/(eval(data_year[1])-eval(data_year[0])+1)
+EHD_9 = EHD_9.groupby('time.day').sum()/(eval(data_year[1])-eval(data_year[0])+1)
+# 掩膜处理
+EHD_6 = masked(EHD_6, r"C:\Users\10574\OneDrive\File\气象数据资料\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp")
+EHD_7 = masked(EHD_7, r"C:\Users\10574\OneDrive\File\气象数据资料\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp")
+EHD_8 = masked(EHD_8, r"C:\Users\10574\OneDrive\File\气象数据资料\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp")
+EHD_9 = masked(EHD_9, r"C:\Users\10574\OneDrive\File\气象数据资料\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp")
+print("数据处理完成")
+pass
