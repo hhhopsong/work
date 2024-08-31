fe273a0bcbff40da04f085b73b07767a8f9f80d0
hhhopsong <1057422494@qq.com>
2024-08-31 16:32:48 +0800

    🚀

--- Meteorological/toolbar/significance_test.py
+++ Meteorological/toolbar/significance_test.py
@@ -2,14 +2,14 @@ import numpy as np
 from scipy.stats import t
 
 
-def t_test(n, pc, data, p=0.95):
+def t_test(pc, data, p=0.95):
     """
-    :param n: 自由度
     :param pc: 时间序列
     :param data: 数据
     :param p: 置信度
     :return: 显著性检验结果
     """
+    n = len(pc)
     Lxx = np.sum((pc - np.mean(pc)) ** 2)
     Sr = data ** 2 * Lxx
     St = np.sum((data - np.mean(data, axis=0)) ** 2, axis=0)
