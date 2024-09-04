372822eb1a1a084b92c0586333ac663dbdc6de5e
hhhopsong <1057422494@qq.com>
2024-09-04 12:08:28 +0800

    🚀

---
+++ Meteorological/paper1/全球降水因子备择.py
@@ -0,0 +1,109 @@
+from cartopy import crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.io.shapereader import Reader
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter  # 专门提供经纬度的
+import numpy as np
+import pymannkendall as mk
+import xarray as xr
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mtransforms
+from matplotlib import ticker
+from matplotlib.ticker import MultipleLocator, FixedLocator
+from matplotlib import gridspec
+import matplotlib.colors as colors
+from cnmaps import get_adm_maps, draw_maps
+import cmaps
+from toolbar.masked import masked  # 气象工具函数
+from toolbar.sub_adjust import adjust_sub_axes
+from toolbar.pre_whitening import ws2001
+from toolbar.significance_test import corr_test
+import seaborn as sns
+import tqdm
+import multiprocessing
+
+
+# 多核计算部分函数
+def multi_core(num, m1, m2, ols, sen):
+    print(f"第{num}个相关系数计算中...")
+    pre_diff = xr.open_dataset(fr"cache\glopre_diff\pre_{num}_{m1}_{m2}.nc")['pre'].transpose('lat', 'lon', 'time')  # 读取缓存
+    try:
+        corr_1 = np.load(fr"cache\corr_glopre_1\corr_{num}_{m1}_{m2}.npy")  # 读取缓存
+    except:
+        corr_1 = np.array([[np.corrcoef(ols, pre_diff.sel(lat=ilat, lon=ilon))[0, 1] for ilon in pre_diff['lon']] for ilat in pre_diff['lat']])
+        np.save(fr"cache\corr_glopre_1\corr_{num}_{m1}_{m2}.npy", corr_1)  # 保存缓存
+    try:
+        corr_2 = np.load(fr"cache\corr_glopre_2\corr_{num}_{m1}_{m2}.npy")  # 读取缓存
+    except:
+        corr_2 = np.array([[np.corrcoef(sen, pre_diff.sel(lat=ilat, lon=ilon))[0, 1] for ilon in pre_diff['lon']] for ilat in pre_diff['lat']])
+        np.save(fr"cache\corr_glopre_2\corr_{num}_{m1}_{m2}.npy", corr_2)
+    print(f"第{num}个相关系数完成。")
+
+
+if __name__ == '__main__':
+    # 数据读取
+    ols = np.load(r"cache\OLS_detrended.npy")  # 读取缓存
+    sen = np.load(r"cache\SEN_detrended.npy")  # 读取缓存
+    num = 0
+    M = 6  # 临界月
+    Ncpu = multiprocessing.cpu_count()
+    data_pool = []
+    # 多核计算
+    for x in range(11, -1, -1):
+        m1 = M + x + 1
+        if m1 > 12:
+            m1 -= 12
+        for y in range(11 - x, 12):
+            num += 1
+            m2 = M - y
+            if m2 <= 0:
+                m2 += 12
+            data_pool.append([num, m1, m2, ols, sen])
+
+    p = multiprocessing.Pool()
+    p.starmap(multi_core, data_pool)
+    p.close()
+    p.join()
+
+    # 绘图
+    # ##地图要素设置
+    plt.rcParams['font.sans-serif'] = ['Arial']
+    plt.rcParams['axes.unicode_minus'] = False
+    fig = plt.figure(figsize=(16, 9))  # 创建画布
+    spec = gridspec.GridSpec(nrows=12, ncols=12)  # 设置子图比例
+    lev = [i*.05 for i in range(-15, 16, 1)]
+    select = eval(input("选择回归方案(1 OLS 2 SEN):"))
+    num = 0
+    draw_pool = []
+    for x in range(11, -1, -1):
+        m1 = M + x + 1
+        if m1 > 12:
+            m1 -= 12
+        for y in tqdm.trange(11 - x, 12):
+            num += 1
+            m2 = M - y
+            if m2 <= 0:
+                m2 += 12
+            pre_diff = xr.open_dataset(fr"cache\glopre_diff\pre_{num}_{m1}_{m2}.nc")['pre'].transpose('lat', 'lon', 'time')
+            corr_1 = np.load(fr"cache\corr_glopre_1\corr_{num}_{m1}_{m2}.npy")  # 读取缓存
+            corr_2 = np.load(fr"cache\corr_glopre_2\corr_{num}_{m1}_{m2}.npy")  # 读取缓存
+            if select == 1:
+                corr = corr_1
+                显著性检验结果 = corr_test(ols, corr, alpha=0.05)
+            elif select == 2:
+                corr = corr_2
+                显著性检验结果 = corr_test(sen, corr, alpha=0.05)
+            ax = fig.add_subplot(spec[y, x], projection=ccrs.PlateCarree(central_longitude=180))
+            相关系数图层 = ax.contourf(pre_diff['lon'], pre_diff['lat'], corr, levels=lev,
+                                       cmap=cmaps.MPL_RdYlGn[32:56] + cmaps.CBR_wet[0] + cmaps.MPL_RdYlGn[72:96],
+                                       extend='both',
+                                       transform=ccrs.PlateCarree())
+            显著性检验结果 = np.where(显著性检验结果 == 1, 0, np.nan)
+            显著性检验图层 = ax.quiver(pre_diff['lon'], pre_diff['lat'], 显著性检验结果, 显著性检验结果, scale=20,
+                                       color='black', headlength=2, headaxislength=2, regrid_shape=60,
+                                       transform=ccrs.PlateCarree(central_longitude=0))
+            ax.set_extent([-180, 180, -30, 80], crs=ccrs.PlateCarree(central_longitude=180))
+            ax.add_feature(cfeature.COASTLINE.with_scale('10m'), linewidth=0.05)
+            draw_maps(get_adm_maps(level='国'), linewidth=0.15)
+
+    plt.savefig(fr"C:\Users\10574\Desktop\glopre_corr{select}.png", dpi=2000, bbox_inches='tight')
+    plt.show()
--- Meteorological/paper1/数据预处理及计算.py
+++ Meteorological/paper1/数据预处理及计算.py
@@ -118,7 +118,7 @@ if eval(input("5)是否计算海温时间滚动差值(0/1)?\n")):
                 sst_output.to_netcdf(fr"D:\CODES\Python\Meteorological\paper1\cache\sst_diff\sst_{times+1}_{M}_{m}.nc")
                 times += 1
                 del sst_output, sst_forward, sst_backfore
-if eval(input("6)是否计算降水时间滚动差值(0/1)?\n")):
+if eval(input("6)是否计算陆地降水时间滚动差值(0/1)?\n")):
     key_month = 6  # 关键月份(临期月份),距离研究时段最近的前向月份
     pre = xr.open_dataset(r"E:\data\CRU\grid\pre\cru_ts4.08.1901.2023.pre.dat.nc")['pre']
     pre = pre.sel(time=slice(str(eval(data_year[0]) - 1) + '-01-01', str(eval(data_year[1]) + 1) + '-12-31'))
@@ -159,6 +159,49 @@ if eval(input("6)是否计算降水时间滚动差值(0/1)?\n")):
                                                               ('lat', forward['lat'].data),
                                                               ('lon', forward['lon'].data)]).to_dataset(name='pre')
                 output.to_netcdf(fr"D:\CODES\Python\Meteorological\paper1\cache\pre_diff\pre_{times+1}_{M}_{m}.nc")
+                times += 1
+                del output, forward, backfore
+if eval(input("7)是否计算全球降水时间滚动差值(0/1)?\n")):
+    key_month = 6  # 关键月份(临期月份),距离研究时段最近的前向月份
+    pre = xr.open_dataset(r"E:\data\NOAA\PREC\precip.mon.anom.nc")['precip']
+    pre = pre.sel(time=slice(str(eval(data_year[0]) - 1) + '-01-01', str(eval(data_year[1]) + 1) + '-12-31'))
+    times = 0
+    # 研究月份外时间滚动差值(不含同期!!)
+    for m1 in range(0, 12):
+        M = key_month - m1  # 前向月份
+        M_cross = 0  # 前向月份跨年标志
+        if M <= 0:  # 向前跨年
+            M += 12
+            M_cross = 1
+        for m2 in range(0, 12-m1):
+            if m2 == 0:
+                if M_cross == 0:
+                    output = pre.sel(time=slice(str(eval(data_year[0])) + '-01-01', str(eval(data_year[1])) + '-12-31'))
+                    output = output.sel(time=output['time.month'].isin([M]))
+                    output.to_netcdf(fr"D:\CODES\Python\Meteorological\paper1\cache\glopre_diff\pre_{times+1}_{M}_{M}.nc")
+                    times += 1
+                    del output
+                elif M_cross == 1:
+                    output = pre.sel(time=slice(str(eval(data_year[0]) - 1) + '-01-01', str(eval(data_year[1]) - 1) + '-12-31'))
+                    output = output.sel(time=output['time.month'].isin([M]))
+                    output.to_netcdf(fr"D:\CODES\Python\Meteorological\paper1\cache\glopre_diff\pre_{times+1}_{M}_{M}.nc")
+                    times += 1
+                    del output
+            else:
+                m = M - m2  # 后向月份
+                m_cross = M_cross  # 后向月份跨年标志(为何直接用=M_cross? 因为前向月份已跨年,后向月份必跨年)
+                if m <= 0:
+                    m += 12
+                    m_cross = 1
+                forward = pre.sel(time=slice(str(eval(data_year[0]) - M_cross) + '-01-01', str(eval(data_year[1]) - M_cross) + '-12-31'))
+                forward = forward.sel(time=forward['time.month'].isin([M]))
+                backfore = pre.sel(time=slice(str(eval(data_year[0]) - m_cross) + '-01-01', str(eval(data_year[1]) - m_cross) + '-12-31'))
+                backfore = backfore.sel(time=backfore['time.month'].isin([m]))
+                output = forward.to_numpy() - backfore.to_numpy()
+                output = xr.DataArray(output.data, coords=[('time', forward['time.year'].data),
+                                                              ('lat', forward['lat'].data),
+                                                              ('lon', forward['lon'].data)]).to_dataset(name='pre')
+                output.to_netcdf(fr"D:\CODES\Python\Meteorological\paper1\cache\glopre_diff\pre_{times+1}_{M}_{m}.nc")
                 times += 1
                 del output, forward, backfore
 print("数据处理完成")
