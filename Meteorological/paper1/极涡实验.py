0d5ba287c2a50c2f59ee6d30ea568a2e6ba22a43
Hopsong <1057422494@qq.com>
2024-09-18 00:01:14 +0800

    🚀

--- Meteorological/paper1/极涡实验.py
+++ Meteorological/paper1/极涡实验.py
@@ -7,35 +7,60 @@ from LBM.force_file import horizontal_profile as hp
 from LBM.force_file import vertical_profile as vp
 from LBM.force_file import mk_grads, mk_wave, interp3d_lbm
 
+from metpy.units import units
 import metpy.calc as mpcalc
+from metpy.xarray import grid_deltas_from_dataarray
 
 import xarray as xr
 import numpy as np
 
+from toolbar.significance_test import corr_test
 
+info = xr.open_dataset(r"D:\CODES\Python\Meteorological\paper1\cache\uvz\z\diff\z_24_4_4.nc")
+z200 = np.load(r"D:\CODES\Python\Meteorological\paper1\cache\uvz\z\corr1\corr_200_24_4_4.npy")
+z500 = np.load(r"D:\CODES\Python\Meteorological\paper1\cache\uvz\z\corr1\corr_500_24_4_4.npy")
+z600 = np.load(r"D:\CODES\Python\Meteorological\paper1\cache\uvz\z\corr1\corr_600_24_4_4.npy")
+z700 = np.load(r"D:\CODES\Python\Meteorological\paper1\cache\uvz\z\corr1\corr_700_24_4_4.npy")
+z850 = np.load(r"D:\CODES\Python\Meteorological\paper1\cache\uvz\z\corr1\corr_850_24_4_4.npy")
+frc = xr.Dataset({'z':(['lev', 'lat', 'lon'], np.array([z850, z700, z600, z500, z200]))},
+                 coords={'lev': [850, 700, 600, 500, 200], 'lat': info['lat'], 'lon': info['lon']})
+# 读取强迫场
+# 选择45-90N，35W-35E的区域
+ols = np.load(r"cache\OLS_detrended.npy")  # 读取缓存
+lon, lat = np.meshgrid(frc['lon'], frc['lat'])
+mask = ((np.where(lon<= 35, 1, 0) + np.where(lon>= 325, 1, 0))
+        * (np.where(lat>= 45, 1, 0) * np.where(lat<= 80, 1, 0))
+        * np.where(frc['z'] <= 0, 1, 0)
+        * corr_test(ols, frc['z'], alpha=0.05, other=0))
+frc_mask = frc.where(mask != 0, 0)
 
-frc = xr.open_dataset(r"D:\CODES\Python\Meteorological\paper1\cache\uvz\z\diff\z_24_4_4.nc")# 读取强迫场
-frc_zone = frc.sel(lon=slice(-35, 35), lat=slice(45, 80))  # 选择区域
+heights = frc_mask['z'] * units('m^2/s^2')
+dx, dy = mpcalc.lat_lon_grid_deltas(np.meshgrid(heights.lon, heights.lat)[0], np.meshgrid(heights.lon, heights.lat)[1])
 
-heights = frc['z']  # 等压位势高度
-f = mpcalc.coriolis_parameter(frc['lat'])  # 包含运动学参数的计算(例如，散度或涡度)
-dx, dy = mpcalc.grid_deltas_from_dataarray(heights)
-ug, vg = mpcalc.geostrophic_wind(heights, f, dx, dy)  # 计算从高度或重力势给出的地转风
-vert_abs_vort = f + mpcalc.vorticity(ug, vg, dx, dy)  # 计算水平风的垂直涡度
+vor = np.zeros_like(heights)
+# 计算相对涡度
+p=0
+for i in [850, 700, 600, 500, 200]:
+    ug, vg = mpcalc.geostrophic_wind(heights.sel(lev=i)*(np.std(info['z'].sel(p=i), axis=0)/np.std(ols)), dx=dx, dy=dy) # 计算从高度或重力势给出的地转风
+    vor[p] = mpcalc.vorticity(ug, vg, dx=dx, dy=dy)  # 计算水平风的垂直涡度
+    p += 1
 
-frc_nc_sigma = interp3d_lbm(frc)
-frc_nc_p = interp3d_lbm(frc, 'p')
+vor = xr.Dataset({'v':(['lev', 'lat', 'lon'], vor)},
+                    coords={'lev': [850, 700, 600, 500, 200], 'lat': info['lat'], 'lon': info['lon']})
+frc_nc_sigma = interp3d_lbm(vor)
+frc_nc_p = interp3d_lbm(vor, 'p')
 # 绘图
 # 图1
-lev = 500
+var = 'v' #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+lev = 200
 n = 10
 extent1 = [-180, 180, -80, 80]
 fig = plt.figure(figsize=(10, 5))
 ax1 = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
 ax1.coastlines(linewidths=0.3)
 ax1.set_extent(extent1, crs=ccrs.PlateCarree())
-lev_range = np.linspace(-np.max(np.abs(frc_nc_p['t'].sel(lev=lev).data)), np.max(np.abs(frc_nc_p['t'].sel(lev=lev).data)), 10)
-t200 = ax1.contourf(frc_nc_p['t']['lon'], frc_nc_p['t']['lat'], frc_nc_p['t'].sel(lev=lev),
+lev_range = np.linspace(-np.max(np.abs(frc_nc_p[var].sel(lev=lev).data)), np.max(np.abs(frc_nc_p[var].sel(lev=lev).data)), 10)
+t200 = ax1.contourf(frc_nc_p[var]['lon'], frc_nc_p[var]['lat'], frc_nc_p[var].sel(lev=lev),
                     levels=lev_range, cmap=cmaps.GMT_polar, transform=ccrs.PlateCarree(central_longitude=0), extend='both')
 plt.show()
 
