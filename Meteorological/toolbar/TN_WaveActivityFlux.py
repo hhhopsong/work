5f79a6166654d9dbf664968ed4602680c6b0ab44
hopsong <1057422494@qq.com>
2024-11-21 16:48:59 +0800

    更新TN_WaveActivityFlux.py文档说明
    将函数TN_WAF_3D的文档字符串中的“DataArray”更正为“3D-DataArray”，以明确输入数据的维度要求。

--- Meteorological/toolbar/TN_WaveActivityFlux.py
+++ Meteorological/toolbar/TN_WaveActivityFlux.py
@@ -157,7 +157,7 @@ def TN_WAF(Geopotential_climatic, U_climatic, V_climatic, Geopotential, lon=np.a
 
 def TN_WAF_3D(GEOc, Uc, Vc, GEOa, Tc=None, u_threshold=5, return_streamf=False, filt=1):
     """
-    计算的是三维的TN波作用通量, 请注意输入的数据格式为DataArray,代码参考了下列样例,并做了勘误。\n
+    计算的是三维的TN波作用通量, 请注意输入的数据格式为3D-DataArray,代码参考了下列样例,并做了勘误。\n
     https://www.bilibili.com/read/cv15633261/?spm_id_from=333.999.collection.opus.click
     :param GEOc:    气候态位势高度场
     :param Uc:    气候态U风
