ca059ef233d1b5e9bec84b1edea6d4e0e2b0e242
hopsong <1057422494@qq.com>
2024-11-26 16:43:37 +0800

    删除 `data_download.py` 并在 `pytorch_test.py` 中添加数据下载功能
    移除了 `toolbar/data_download.py` 文件，并将数据下载功能迁移到了 `pytorch_test.py` 中，同时增加了多进程处理和异常处理机制。此外，还更新了 `test.ipynb` 以简化代码并改进了数据处理逻辑。新增了 `toolbar/download_data.ipynb` 用于数据下载。

--- Meteorological/pytorch_test.py
+++ Meteorological/pytorch_test.py
@@ -1,13 +1,53 @@
-from toolbar.filter import LanczosFilter, ButterworthFilter
-from toolbar.wavelet import *
-from toolbar.filter import *
+import cdsapi
+import tqdm as tq
+import multiprocessing
+import xarray as xr
+
+def download(year):
+    while True:
+        try:
+            try:
+                read_test = xr.open_dataset(f'E:\data\ERA5\ERA5_pressLev\daily\era5_uvz_daily_{year}.zip')
+                print(f"{year} 已存在.")
+                return
+            except:
+                pass
+            print(f"Try to download {year} data...")
+            dataset = "derived-era5-pressure-levels-daily-statistics"
+            request = {
+                "product_type": "reanalysis",
+                "variable": [
+                    "geopotential",
+                    "u_component_of_wind",
+                    "v_component_of_wind"
+                ],
+                "year": [str(year)],
+                "month": ["06", "07", "08"],
+                "day": [
+                    "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                    "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
+                ],
+                "pressure_level": ["200"],
+                "daily_statistic": "daily_mean",
+                "time_zone": "utc+00:00",
+                "frequency": "6_hourly"
+            }
+            target = f'E:\data\ERA5\ERA5_pressLev\daily\era5_uvz_daily_{year}.zip'
+            client = cdsapi.Client()
+            client.retrieve(dataset, request, target)
+            print(f"{year} 下载完成.")
+            return
+        except:
+            pass
 
 
-dat = np.load("D:\PyFile\paper1\OLS35.npy")
-# 滤波
-filter = ButterworthFilter(dat, filter_type="bandpass", filter_window=9, cutoff=[2.5, 6])
-dat = filter.filted()
-# 小波分析
-wavelet_analysis = WaveletAnalysis(dat, wave='Morlet', dt=1, detrend=False, normal=True, signal=.90, J=4)
-wavelet_analysis.plot(unit="1")
-
+if __name__ == '__main__':
+    Ncpu = multiprocessing.cpu_count() - 1
+    data_pool = []
+    for var in range(1961, 2025):
+        data_pool.append([var])
+    p = multiprocessing.Pool()
+    p.starmap(download, data_pool)
+    p.close()
+    p.join()
