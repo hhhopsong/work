57f4f88cab9a5ab9fc7a78f20bdff72fe473f740
hopsong <1057422494@qq.com>
2025-02-08 23:50:54 +0800

    upupup

---
+++ Meteorological/p2/数值模拟/frc_plot_温度实验.py
@@ -0,0 +1,207 @@
+import xarray as xr
+import xgrads
+import cmaps
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
+import cartopy.crs as ccrs
+import scipy.ndimage as ndimage
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from cartopy.util import add_cyclic
+import cartopy.feature as cfeature
+from cartopy.io.shapereader import Reader
+import geopandas as gpd
+
+
+from toolbar.curved_quivers.modplot import Curlyquiver
+
+
+def draw_frc():
+    frc_nc_p = xr.open_dataset(r'D:\lbm\main\data\Forcing\frc_p.t42l20.nc')
+    lbm = xr.open_dataset(r'D:\lbm\main\data\Output\Output_frc.t42l20.Tingyang.nc')
+    u = lbm['u'][19:25].mean('time')
+    v = lbm['v'][19:25].mean('time')
+    t = lbm['t'][19:25].mean('time')
+    z = lbm['z'][19:25].mean('time')
+    lon = lbm['lon']
+    lat = lbm['lat']
+    # 绘图
+    # 图1
+    lev = 200
+    lev_Z = np.array([-10, -5, 5, 10]) * 2
+    extent1 = [-180, 180, -30, 80]
+    fig = plt.figure(figsize=(10, 5))
+    ax1 = fig.add_subplot(411, projection=ccrs.PlateCarree(central_longitude=180-67.5))
+    ax1.set_title('a)Exp 200hPa UVZ', fontsize=6, loc='left')
+    ax1.coastlines(linewidths=0.3)
+    ax1.set_extent(extent1, crs=ccrs.PlateCarree())
+    # 强迫
+    var = 't'
+    frc_fill_white, lon_fill_white = add_cyclic(frc_nc_p[var].sel(lev=lev, time=0), frc_nc_p[var]['lon'])
+    lev_range = np.linspace(-np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), 8)
+    var_contr = ax1.contourf(lon_fill_white, frc_nc_p[var]['lat'], frc_fill_white, linewidths=0.5,
+                        levels=lev_range, cmap=cmaps.BlueWhiteOrangeRed[20:-20], transform=ccrs.PlateCarree(central_longitude=0), extend='both')
+    # 响应
+    T, lon_T = t.sel(lev=lev), lon
+    Z, lon_Z = ndimage.gaussian_filter(z.sel(lev=lev), 1), lon
+    U, lon_UV = u.sel(lev=lev), lon
+    V, lon_UV = v.sel(lev=lev), lon
+    z_fill_white, lon_fill_white = add_cyclic(Z, lon_Z)
+    t200_low = ax1.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z < 0], cmap=cmaps.BlueDarkRed18[0],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    t200_high = ax1.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z > 0], cmap=cmaps.BlueDarkRed18[-1],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    wind200 = Curlyquiver(ax1, lon_UV, lat, U, V, arrowsize=.2, scale=20, regrid=20, linewidth=0.25,
+                        color='black', center_lon=180-70)
+    wind200.key(fig, U=0.02, label='0.02 m/s', ud=7.7, edgecolor='none', arrowsize=.5, color='k')
+    # 图2
+    lev = 500
+    lev_Z = np.array([-10, -5, 5, 10]) * 2
+    extent1 = extent1
+    ax2 = fig.add_subplot(412, projection=ccrs.PlateCarree(central_longitude=180-67.5))
+    ax2.set_title('b)Exp 500hPa UVZ', fontsize=6, loc='left')
+    ax2.coastlines(linewidths=0.3)
+    ax2.set_extent(extent1, crs=ccrs.PlateCarree())
+    # 强迫
+    frc_fill_white, lon_fill_white = add_cyclic(frc_nc_p[var].sel(lev=lev, time=0), frc_nc_p[var]['lon'])
+    lev_range = np.linspace(-np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), 8)
+    var_contr = ax2.contourf(lon_fill_white, frc_nc_p[var]['lat'], frc_fill_white, linewidths=0.5,
+                        levels=lev_range, cmap=cmaps.BlueWhiteOrangeRed[20:-20], transform=ccrs.PlateCarree(central_longitude=0), extend='both')
+    # 响应
+    T, lon_T = t.sel(lev=lev), lon
+    Z, lon_Z = ndimage.gaussian_filter(z.sel(lev=lev), 1), lon
+    U, lon_UV = u.sel(lev=lev), lon
+    V, lon_UV = v.sel(lev=lev), lon
+    z_fill_white, lon_fill_white = add_cyclic(Z, lon_Z)
+    t500_low = ax2.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z < 0], cmap=cmaps.BlueDarkRed18[0],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    t500_high = ax2.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z > 0], cmap=cmaps.BlueDarkRed18[-1],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    wind500 = Curlyquiver(ax2, lon_UV, lat, U, V, arrowsize=.2, scale=20, regrid=20, linewidth=0.25,
+                        color='black', center_lon=180-70)
+    wind500.key(fig, U=0.02, label='0.02 m/s', ud=7.7, edgecolor='none', arrowsize=.5, color='k')
+
+    # 图1
+    lev = 700
+    lev_Z = np.array([-10, -5, 5, 10]) * 1
+    extent1 = extent1
+    ax3 = fig.add_subplot(413, projection=ccrs.PlateCarree(central_longitude=180-67.5))
+    ax3.set_title('c)Exp 700hPa UVZ', fontsize=6, loc='left')
+    ax3.coastlines(linewidths=0.3)
+    ax3.set_extent(extent1, crs=ccrs.PlateCarree())
+    # 强迫
+    frc_fill_white, lon_fill_white = add_cyclic(frc_nc_p[var].sel(lev=lev, time=0), frc_nc_p[var]['lon'])
+    lev_range = np.linspace(-np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), 8)
+    var_contr = ax3.contourf(lon_fill_white, frc_nc_p[var]['lat'], frc_fill_white, linewidths=0.5,
+                        levels=lev_range, cmap=cmaps.BlueWhiteOrangeRed[20:-20], transform=ccrs.PlateCarree(central_longitude=0), extend='both')
+    # 响应
+    T, lon_T = t.sel(lev=lev), lon
+    Z, lon_Z = ndimage.gaussian_filter(z.sel(lev=lev), 1), lon
+    U, lon_UV = u.sel(lev=lev), lon
+    V, lon_UV = v.sel(lev=lev), lon
+    z_fill_white, lon_fill_white = add_cyclic(Z, lon_Z)
+    t700_low = ax3.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z < 0], cmap=cmaps.BlueDarkRed18[0],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    t700_high = ax3.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z > 0], cmap=cmaps.BlueDarkRed18[-1],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    wind850 = Curlyquiver(ax3, lon_UV, lat, U, V, arrowsize=.2, scale=20, regrid=20, linewidth=0.25,
+                        color='black', center_lon=180-70)
+    wind850.key(fig, U=0.02, label='0.02 m/s', ud=7.7, edgecolor='none', arrowsize=.5, color='k')
+    DBATP = r"D:\PyFile\map\地图边界数据\青藏高原边界数据总集\TPBoundary_2500m\TPBoundary_2500m.shp"
+    provinces = cfeature.ShapelyFeature(Reader(DBATP).geometries(), crs=ccrs.PlateCarree(), facecolor='gray', alpha=1)
+    ax3.add_feature(provinces, lw=0.5, zorder=2)
+    # 图1
+    lev = 850
+    lev_Z = np.array([-10, -5, 5, 10]) * 0.2
+    extent1 = extent1
+    ax4 = fig.add_subplot(414, projection=ccrs.PlateCarree(central_longitude=180-67.5))
+    ax4.set_title('d)Exp 850hPa UVZ', fontsize=6, loc='left')
+    ax4.coastlines(linewidths=0.3)
+    ax4.set_extent(extent1, crs=ccrs.PlateCarree())
+    # 强迫
+    frc_fill_white, lon_fill_white = add_cyclic(frc_nc_p[var].sel(lev=lev, time=0), frc_nc_p[var]['lon'])
+    lev_range = np.linspace(-np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), np.nanmax(np.abs(frc_nc_p[var].sel(lev=lev).data)), 8)
+    var_contr = ax4.contourf(lon_fill_white, frc_nc_p[var]['lat'], frc_fill_white,  linewidths=0.5,
+                        levels=lev_range, cmap=cmaps.BlueWhiteOrangeRed[40:126], transform=ccrs.PlateCarree(central_longitude=0), extend='both')
+    # 响应
+    T, lon_T = t.sel(lev=lev), lon
+    Z, lon_Z = ndimage.gaussian_filter(z.sel(lev=lev), 1), lon
+    U, lon_UV = u.sel(lev=lev), lon
+    V, lon_UV = v.sel(lev=lev), lon
+    z_fill_white, lon_fill_white = add_cyclic(Z, lon_Z)
+    t850_low = ax4.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z < 0], cmap=cmaps.BlueDarkRed18[0],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    t850_high = ax4.contour( lon_fill_white, lat, z_fill_white, levels=lev_Z[lev_Z > 0], cmap=cmaps.BlueDarkRed18[-1],
+                        transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.2)
+    #z850 = ax4.contour(lon_Z, lat, Z, levels=4, colors='black', transform=ccrs.PlateCarree(central_longitude=0), linewidths=0.4)
+    wind850 = Curlyquiver(ax4, lon_UV, lat, U, V, arrowsize=.2, scale=20, regrid=20, linewidth=0.25,
+                        color='black', center_lon=180-70)
+    wind850.key(fig, U=0.02, label='0.02 m/s', ud=7.7, edgecolor='none', arrowsize=.5, color='k')
+    DBATP = r"D:\PyFile\map\地图边界数据\青藏高原边界数据总集\TPBoundary_2500m\TPBoundary_2500m.shp"
+    provinces = cfeature.ShapelyFeature(Reader(DBATP).geometries(), crs=ccrs.PlateCarree(), facecolor='gray', alpha=1)
+    ax4.add_feature(provinces, lw=0.5, zorder=2)
+
+
+    # 刻度线设置
+    xticks1 = np.arange(extent1[0], extent1[1] + 1, 10)
+    yticks1 = np.arange(extent1[2], extent1[3] + 1, 10)
+    lon_formatter = LongitudeFormatter()
+    lat_formatter = LatitudeFormatter()
+    # ax1
+    ax1.set_yticks(yticks1, crs=ccrs.PlateCarree())
+    ax1.yaxis.set_major_formatter(lat_formatter)
+    # ax2
+    ax2.set_yticks(yticks1, crs=ccrs.PlateCarree())
+    ax2.yaxis.set_major_formatter(lat_formatter)
+    # ax3
+    ax3.set_yticks(yticks1, crs=ccrs.PlateCarree())
+    ax3.yaxis.set_major_formatter(lat_formatter)
+    # ax4
+    ax4.set_xticks(xticks1, crs=ccrs.PlateCarree())
+    ax4.set_yticks(yticks1, crs=ccrs.PlateCarree())
+    ax4.xaxis.set_major_formatter(lon_formatter)
+    ax4.yaxis.set_major_formatter(lat_formatter)
+
+    xmajorLocator = ticker.MultipleLocator(60)  # 先定义xmajorLocator，再进行调用
+    xminorLocator = ticker.MultipleLocator(10)
+    ymajorLocator = ticker.MultipleLocator(30)
+    yminorLocator = ticker.MultipleLocator(10)
+    ax4.xaxis.set_major_locator(xmajorLocator)  # x轴最大刻度
+    ax4.xaxis.set_minor_locator(xminorLocator)  # x轴最小刻度
+
+    ax1.yaxis.set_major_locator(ymajorLocator)  # y轴最大刻度
+    ax2.yaxis.set_major_locator(ymajorLocator)  # y轴最大刻度
+    ax3.yaxis.set_major_locator(ymajorLocator)  # y轴最大刻度
+    ax4.yaxis.set_major_locator(ymajorLocator)  # y轴最大刻度
+
+    ax1.yaxis.set_minor_locator(yminorLocator)  # y轴最小刻度
+    ax2.yaxis.set_minor_locator(yminorLocator)  # y轴最小刻度
+    ax3.yaxis.set_minor_locator(yminorLocator)  # y轴最小刻度
+    ax4.yaxis.set_minor_locator(yminorLocator)  # y轴最小刻度
+    # ax1.axes.xaxis.set_ticklabels([]) ##隐藏刻度标签
+    # 最大刻度、最小刻度的刻度线长短，粗细设置
+    ax1.tick_params(which='major', length=4, width=.3, color='darkgray')  # 最大刻度长度，宽度设置，
+    ax1.tick_params(which='minor', length=2, width=.3, color='darkgray')  # 最小刻度长度，宽度设置
+    ax1.tick_params(which='both', bottom=True, top=False, left=True, labelbottom=True, labeltop=False)
+    ax2.tick_params(which='major', length=4, width=.3, color='darkgray')  # 最大刻度长度，宽度设置，
+    ax2.tick_params(which='minor', length=2, width=.3, color='darkgray')  # 最小刻度长度，宽度设置
+    ax2.tick_params(which='both', bottom=True, top=False, left=True, labelbottom=True, labeltop=False)
+    ax3.tick_params(which='major', length=4, width=.3, color='darkgray')  # 最大刻度长度，宽度设置，
+    ax3.tick_params(which='minor', length=2, width=.3, color='darkgray')  # 最小刻度长度，宽度设置
+    ax3.tick_params(which='both', bottom=True, top=False, left=True, labelbottom=True, labeltop=False)
+    ax4.tick_params(which='major', length=4, width=.3, color='darkgray')  # 最大刻度长度，宽度设置，
+    ax4.tick_params(which='minor', length=2, width=.3, color='darkgray')  # 最小刻度长度，宽度设置
+    ax4.tick_params(which='both', bottom=True, top=False, left=True, labelbottom=True, labeltop=False)
+    plt.rcParams['xtick.direction'] = 'out'  # 将x轴的刻度线方向设置向内或者外
+    # 调整刻度值字体大小
+    ax1.tick_params(axis='both', labelsize=6, colors='black')
+    ax2.tick_params(axis='both', labelsize=6, colors='black')
+    ax3.tick_params(axis='both', labelsize=6, colors='black')
+    ax4.tick_params(axis='both', labelsize=6, colors='black')
+
+    plt.savefig(r'D:\PyFile\p2\pic\Output_温度实验.png', dpi=600, bbox_inches='tight')
+    plt.show()
+
+
+if __name__ == '__main__':
+    draw_frc()
---
+++ Meteorological/p2/数值模拟/温度实验.py
@@ -0,0 +1,202 @@
+import multiprocessing
+
+import cartopy.crs as ccrs
+import cmaps
+import tqdm
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from cartopy.util import add_cyclic
+
+from matplotlib import pyplot as plt, gridspec
+from matplotlib.ticker import MultipleLocator, FixedLocator, ScalarFormatter
+
+from metpy.units import units
+import metpy.calc as mpcalc
+from metpy.xarray import grid_deltas_from_dataarray
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+from toolbar.curved_quivers.modplot import Curlyquiver
+from scipy.ndimage import filters
+
+import xarray as xr
+import xgrads as xg
+import numpy as np
+
+from toolbar.LBM.force_file import horizontal_profile as hp
+from toolbar.LBM.force_file import vertical_profile as vp
+from toolbar.LBM.force_file import mk_grads, mk_wave, interp3d_lbm
+from toolbar.significance_test import corr_test
+
+
+def corr(time_series, data):
+    # 计算相关系数
+    # 将 data 重塑为二维：时间轴为第一个维度
+    reshaped_data = data.reshape(len(time_series), -1)
+
+    # 减去均值以标准化
+    time_series_mean = time_series - np.mean(time_series)
+    data_mean = reshaped_data - np.mean(reshaped_data, axis=0)
+
+    # 计算分子（协方差）
+    numerator = np.sum(data_mean * time_series_mean[:, np.newaxis], axis=0)
+
+    # 计算分母（标准差乘积）
+    denominator = np.sqrt(np.sum(data_mean ** 2, axis=0)) * np.sqrt(np.sum(time_series_mean ** 2))
+
+    # 相关系数
+    correlation = numerator / denominator
+
+    # 重塑为 (lat, lon)
+    correlation_map = correlation.reshape(data.shape[1:])
+    return correlation_map
+
+def regress(time_series, data):
+    # 将 data 重塑为二维：时间轴为第一个维度
+    reshaped_data = data.reshape(len(time_series), -1)
+
+    # 减去均值以中心化（标准化自变量和因变量）
+    time_series_mean = time_series - np.mean(time_series)
+    data_mean = reshaped_data - np.mean(reshaped_data, axis=0)
+
+    # 计算分子（协方差的分子）
+    numerator = np.sum(data_mean * time_series_mean[:, np.newaxis], axis=0)
+
+    # 计算分母（自变量的平方和）
+    denominator = np.sum(time_series_mean ** 2)
+
+    # 计算回归系数
+    regression_coef = numerator / denominator
+    correlation = numerator / (np.sqrt(np.sum(data_mean ** 2, axis=0)) * np.sqrt(np.sum(time_series_mean ** 2)))
+    # 重塑为 (lat, lon)
+    regression_map = regression_coef.reshape(data.shape[1:])
+    correlation_map = correlation.reshape(data.shape[1:])
+    return regression_map, correlation_map
+
+if __name__ == '__main__':
+    info_t = xr.open_dataset(r"D:/PyFile/p2/data/T.nc")['t']
+    info_sst = xr.open_dataset(r"D:/PyFile/p2/data/SST.nc").interp(lat=info_t['lat'], lon=info_t['lon'])['sst']
+    K_type = xr.open_dataset(r"D:/PyFile/p2/data/Time_type_AverFiltAll0.9%_0.3%_3.nc")
+    K_series = K_type.sel(type=1)['K'].data
+    K_series = K_series - np.polyval(np.polyfit(range(len(K_series)), K_series, 1), range(len(K_series)))
+    K_series = (K_series - np.mean(K_series)) / np.std(K_series)
+
+    zone = [360 - 65, 360 - 10, 65, 30]
+    corr_weight = corr(K_series, info_sst.data)
+    corr_weight_1times = 1 / np.nanmean(corr(K_series, info_sst.sel(lon=slice(zone[0], zone[1]), lat=slice(zone[2], zone[3])).data))
+    corr_weight = corr_weight * corr_weight_1times
+    ols = K_series # 读取缓存
+
+    t1000 = np.nan_to_num(regress(ols, info_t.sel(level=1000).data)[0], nan=0)
+    t850 = np.nan_to_num(regress(ols, info_t.sel(level=850).data)[0], nan=0)
+    t500 = np.nan_to_num(regress(ols, info_t.sel(level=500).data)[0], nan=0)
+    t200 = np.nan_to_num(regress(ols, info_t.sel(level=200).data)[0], nan=0)
+    t150 = np.nan_to_num(regress(ols, info_t.sel(level=150).data)[0], nan=0)
+    t100 = np.nan_to_num(regress(ols, info_t.sel(level=100).data)[0], nan=0)
+    frc = xr.Dataset({'t':(['lev', 'lat', 'lon'],
+                           np.array([t1000, t850, t500, t200, t150, t100]))},
+                     coords={'lev': [1000, 850, 500, 200, 150, 100],
+                             'lat': info_t['lat'],
+                             'lon': info_t['lon']})
+    # 读取强迫场
+    T = frc * corr_weight * units('K')
+    lon, lat = np.meshgrid(frc['lon'], frc['lat'])
+    mask = ((np.where(lon<= zone[1], 1, 0) * np.where(lon>= zone[0], 1, 0))
+            * (np.where(lat>= zone[3], 1, 0) * np.where(lat<= zone[2], 1, 0))
+            * np.where(T['t'] >= 0, 1, 0))
+
+    T_mask = T.where(mask != 0, 0)
+
+    frc_nc_sigma = interp3d_lbm(T_mask)
+    frc_nc_p = interp3d_lbm(T_mask, 'p')
+    # 绘图
+    # 图1
+    var = 't' #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    lev = 500
+    n = 10
+    extent1 = [-180, 180, -30, 80]
+    fig = plt.figure(figsize=(10, 5), constrained_layout=True)
+    ax1 = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180-70))
+    ax1.coastlines(linewidths=0.3)
+    ax1.set_extent(extent1, crs=ccrs.PlateCarree())
+    frc_fill_white, lon_fill_white = add_cyclic(frc_nc_p[var].sel(lev=lev, time=0), frc_nc_p[var]['lon'])
+    lev_range = np.linspace(0, np.max(np.abs(frc_nc_p[var].sel(lev=lev).data)), 10)
+    var200 = ax1.contourf(lon_fill_white, frc_nc_p[var]['lat'], frc_fill_white,
+                        levels=lev_range, cmap=cmaps.BlueWhiteOrangeRed[126:-40], transform=ccrs.PlateCarree(central_longitude=0), extend='both')
+    # 刻度线设置
+    xticks1 = np.arange(extent1[0], extent1[1] + 1, 10)
+    yticks1 = np.arange(extent1[2], extent1[3] + 1, 10)
+    lon_formatter = LongitudeFormatter()
+    lat_formatter = LatitudeFormatter()
+    # ax1
+    ax1.set_xticks(xticks1, crs=ccrs.PlateCarree())
+    ax1.xaxis.set_major_formatter(lon_formatter)
+    ax1.set_yticks(yticks1, crs=ccrs.PlateCarree())
+    ax1.yaxis.set_major_formatter(lat_formatter)
+
+    xmajorLocator = MultipleLocator(60)  # 先定义xmajorLocator，再进行调用
+    xminorLocator = MultipleLocator(10)
+    ymajorLocator = MultipleLocator(30)
+    yminorLocator = MultipleLocator(10)
+
+    ax1.yaxis.set_major_locator(ymajorLocator)  # y轴最大刻度
+    ax1.yaxis.set_minor_locator(yminorLocator)  # y轴最小刻度
+    ax1.xaxis.set_major_locator(xmajorLocator)  # x轴最大刻度
+    ax1.xaxis.set_minor_locator(xminorLocator)  # x轴最小刻度
+    # ax1.axes.xaxis.set_ticklabels([]) ##隐藏刻度标签
+    # 最大刻度、最小刻度的刻度线长短，粗细设置
+    ax1.tick_params(which='major', length=11, width=2, color='darkgray')  # 最大刻度长度，宽度设置，
+    ax1.tick_params(which='minor', length=8, width=1.8, color='darkgray')  # 最小刻度长度，宽度设置
+    ax1.tick_params(which='both', bottom=True, top=False, left=True, labelbottom=True, labeltop=False)
+    plt.rcParams['xtick.direction'] = 'out'  # 将x轴的刻度线方向设置向内或者外
+    # 调整刻度值字体大小
+    ax1.tick_params(axis='both', labelsize=12, colors='black')
+    # ax2 垂直层结
+    ax_ins = inset_axes(
+        ax1,
+        width="15%",  # width: 5% of parent_bbox width
+        height="100%",  # height: 50%
+        loc="lower left",
+        bbox_to_anchor=(1.1, 0., 1, 1),
+        bbox_transform=ax1.transAxes,
+        borderpad=0,
+    )
+
+    ax2 = ax_ins
+    S2D = 86400.
+    # 计算各层平均温度
+    avg_temp = T_mask['t'].sel(lon=slice(zone[0], zone[1]), lat=slice(zone[2], zone[3])).mean(dim=['lat', 'lon']).values.squeeze()  # 按纬度和经度平均
+    avg_temp_frc_nc_np = frc_nc_p['t'].sel(lon=slice(zone[0], zone[1]), lat=slice(zone[2], zone[3])).mean(dim=['lat', 'lon']).values.squeeze() * S2D  # frc_nc_p 各层平均温度
+    pressure_levels = frc['lev'].values  # 气压层次
+    pressure_levels_frc_nc_np = frc_nc_p['lev'].values  # frc_nc_p 的气压层次
+
+    # 在 ax2 上绘制
+    ax2.plot(avg_temp, pressure_levels, marker='x', color='gray', label='Obs', alpha=0.7)
+    ax2.plot(avg_temp_frc_nc_np, pressure_levels_frc_nc_np, marker='.', color='r', label='Frc', alpha=0.7)
+
+    # 设置横纵坐标范围
+    ax2.set_ylim(100, 1000)  # 设置横轴范围
+    ax2.set_xlim(-.003, 0.2)  # 设置纵轴范围
+    # 设置纵轴为反转的气压坐标
+    ax2.set_yscale('log')  # 气压通常采用对数坐标
+    ax2.invert_yaxis()  # 倒置 y 轴，使高压在下，低压在上
+
+    # 设置坐标轴标签和标题
+    #ax2.set_xlabel('T/K', fontsize=14)
+    #ax2.set_ylabel('P/hPa', fontsize=14)
+
+    # 设置刻度和网格
+    ax2.yaxis.set_major_locator(FixedLocator([1000, 850, 700, 500, 300, 200, 100]))  # 使用线性刻度标注
+    ax2.yaxis.set_major_formatter(ScalarFormatter())
+    ax2.yaxis.set_minor_locator(FixedLocator([]))  # 设置次要刻度线
+    ax2.yaxis.set_minor_formatter(ScalarFormatter())
+    ax2.grid(which='both', linestyle='--', linewidth=0.5)
+    ax2.tick_params(axis='both', which='major', labelsize=12)
+
+    # 添加图例
+    ax2.legend(fontsize=12)
+    plt.savefig(r"D:\PyFile\p2\pic\frc_data.png", dpi=600, bbox_inches='tight')
+    plt.show()
+
+    if input("是否导出?(1/0)") == '1':
+        frc_nc_sigma.to_netcdf(r'D:\lbm\main\data\Forcing\frc.t42l20.nc', format='NETCDF3_CLASSIC')
+        frc_nc_p.to_netcdf(r'D:\lbm\main\data\Forcing\frc_p.t42l20.nc', format='NETCDF3_CLASSIC')
+
