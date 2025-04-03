31e06ad9bcff780cba7ddd81509c9c89cd2687b3
hopsong <1057422494@qq.com>
2025-04-03 16:56:00 +0800

    upupup

--- Meteorological/考试/地流作业/20250331.py
+++ Meteorological/考试/地流作业/20250331.py
@@ -12,9 +12,14 @@ n0 = 1.
 k = .5
 l = .5
 f = 7.2921 * 10**-5
-sigma = f * 1.25
+sigma = f * 2
 θ = k * X + l * Y
-phi =  2
+phi = 0
+L = 2
+N = 0.15
+Cx = f * 2
+C0 = f * 4
+H0 = 0.01
 
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
@@ -24,14 +29,19 @@ def animate(frame):
     ax.clear()
     t = frame * 10e2  # 时间参数
 
-    n = Re * n0 * np.cos(θ - sigma * t)
-    u = - (9.8 * np.abs(n0)) / (f**2 - sigma**2) * (k * sigma * np.cos(θ - sigma * t + phi) - l * f * np.sin(θ - sigma * t + phi))
-    v = - (9.8 * np.abs(n0)) / (f**2 - sigma**2) * (l * sigma * np.cos(θ - sigma * t + phi) + k * f * np.sin(θ - sigma * t + phi))
+    # # g
+    # n = Re * n0 * np.cos(θ - sigma * t)
+    # u = - (9.8 * np.abs(n0)) / (f**2 - sigma**2) * (k * sigma * np.cos(θ - sigma * t + phi) - l * f * np.sin(θ - sigma * t + phi))
+    # v = - (9.8 * np.abs(n0)) / (f**2 - sigma**2) * (l * sigma * np.cos(θ - sigma * t + phi) + k * f * np.sin(θ - sigma * t + phi))
+
+    n = n0 * (np.cos(N * np.pi * Y / L) - L * f / N / np.pi / Cx * np.sin(N * np.pi * Y / L)) * np.cos(k * X - sigma * t + phi)
+    u = n0 / H0 * (C0**2 / Cx * np.cos(N * np.pi / L * Y) - L * f / N / np.pi * np.sin(N * np.pi / L * Y)) * np.cos(k * X - sigma * t + phi)
+    v = -n0 * L / H0 / sigma / N / np.pi * (f**2 + C0**2 * N**2 * np.pi**2 / L**2) * np.sin(N * np.pi / L * Y) * np.sin(k * X - sigma * t + phi)
 
     surf = ax.plot_surface(X, Y, n, cmap='coolwarm', lw=0.5, rstride=8, cstride=8,alpha=0.4)
 
     ax.contourf(X, Y, n, zdir='z', offset=-6, cmap='coolwarm')
-    ax.quiver(X[::4, ::4], Y[::4, ::4], np.full_like(n, -6)[::4, ::4], u[::4, ::4], v[::4, ::4], np.full_like(n, 0)[::4, ::4], length=0.000004, color='k')
+    ax.quiver(X[::4, ::4], Y[::4, ::4], np.full_like(n, -6)[::4, ::4], u[::4, ::4], v[::4, ::4], np.full_like(n, 0)[::4, ::4], length=0.4, color='k')
     ax.set_xlim(-15, 15)
     ax.set_ylim(-15, 15)
     ax.set_zlim(-6, 2)
