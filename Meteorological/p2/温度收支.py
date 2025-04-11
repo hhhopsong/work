5771312df320f210dbbbc441a39bb1e6f65f14b1
hopsong <1057422494@qq.com>
2025-04-11 10:16:04 +0800

    upupup

--- Meteorological/p2/温度收支.py
+++ Meteorological/p2/温度收支.py
@@ -51,9 +51,9 @@ def regress(time_series, data):
 time = [1961, 2022]
 t_budget = xr.open_dataset(r'E:\data\ERA5\ERA5_pressLev\single_var\t_budget_1961_2022.nc').sel(
     time=slice(str(time[0]) + '-01', str(time[1]) + '-12'))
-dTdt_78 = t_budget['dTdt'].sel(time=t_budget['time.month'].isin([6, 7])).groupby('time.year').mean('time').data
-adv_T_78 = t_budget['adv_T'].sel(time=t_budget['time.month'].isin([7, 8])).groupby('time.year').mean('time').data
-ver_78 = t_budget['ver'].sel(time=t_budget['time.month'].isin([7, 8])).groupby('time.year').mean('time').data
+dTdt_78 = t_budget['dTdt'].sel(time=t_budget['time.month'].isin([6, 7])).groupby('time.year').mean('time')
+adv_T_78 = t_budget['adv_T'].sel(time=t_budget['time.month'].isin([7, 8])).groupby('time.year').mean('time')
+ver_78 = t_budget['ver'].sel(time=t_budget['time.month'].isin([7, 8])).groupby('time.year').mean('time')
 Q_78 = dTdt_78 - adv_T_78 - ver_78
 K_type = xr.open_dataset(r"D:/PyFile/p2/data/Time_type_AverFiltAll0.9%_0.3%_3.nc")
 
@@ -80,14 +80,23 @@ reg_map, corr_map = np.zeros(
     (3, 4, len(t_budget['Q'].level), len(t_budget['Q'].lat), len(t_budget['Q'].lon))), np.zeros(
     (3, 4, len(t_budget['Q'].level), len(t_budget['Q'].lat), len(t_budget['Q'].lon)))
 
+p_lev = 900
+
 for i in tq.trange(len(K_type['type'])):
     K_series = K_type.sel(type=i + 1)['K'].data
-    if i == 1: K_series = K_series - np.polyval(np.polyfit(range(len(K_series)), K_series, 1), range(len(K_series)))
-    K_series = (K_series - np.mean(K_series)) / np.std(K_series)
-    reg_map[i, 0], corr_map[i, 0] = regress(K_series, dTdt_78)
-    reg_map[i, 1], corr_map[i, 1] = regress(K_series, adv_T_78)
-    reg_map[i, 2], corr_map[i, 2] = regress(K_series, ver_78)
-    reg_map[i, 3], corr_map[i, 3] = regress(K_series, Q_78)
+    if i != 1:
+        K_series = (K_series - np.mean(K_series)) / np.std(K_series)
+        reg_map[i, 0], corr_map[i, 0] = regress(K_series, dTdt_78.data)
+        reg_map[i, 1], corr_map[i, 1] = regress(K_series, adv_T_78.data)
+        reg_map[i, 2], corr_map[i, 2] = regress(K_series, ver_78.data)
+        reg_map[i, 3], corr_map[i, 3] = regress(K_series, Q_78.data)
+    else:
+        K_series = K_series[:-1]
+        K_series = (K_series - np.mean(K_series)) / np.std(K_series)
+        reg_map[i, 0], corr_map[i, 0] = regress(K_series, dTdt_78.sel(year=slice(1961, 2021)).data)
+        reg_map[i, 1], corr_map[i, 1] = regress(K_series, adv_T_78.sel(year=slice(1961, 2021)).data)
+        reg_map[i, 2], corr_map[i, 2] = regress(K_series, ver_78.sel(year=slice(1961, 2021)).data)
+        reg_map[i, 3], corr_map[i, 3] = regress(K_series, Q_78.sel(year=slice(1961, 2021)).data)
 
 reg_map = xr.Dataset({'dTdt': (['type', 'level', 'lat', 'lon'], reg_map[:, 0]),
                       'adv_T': (['type', 'level', 'lat', 'lon'], reg_map[:, 1]),
@@ -118,9 +127,9 @@ for KType in range(1, 4):
         reg_map_ = masked(reg_map, r'D:\PyFile\map\self\长江_TP\长江_tp.shp')
     interp_lon = np.linspace(88, 124, 1000)
     interp_lat = np.linspace(22, 38, 500)
-    reg = masked(reg_map.interp(lon=interp_lon, lat=interp_lat), r"D:\PyFile\map\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp").sel(type=KType,level=[850, 900]).mean(dim='level')
-    reg_ = reg_map_.sel(type=KType, level=[850, 900]).mean(dim='level')
-    corr = corr_map.sel(type=KType, level=850)
+    reg = masked(reg_map.interp(lon=interp_lon, lat=interp_lat), r"D:\PyFile\map\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp").sel(type=KType,level=p_lev)
+    reg_ = reg_map_.sel(type=KType, level=p_lev)
+    corr = corr_map.sel(type=KType, level=p_lev)
     # 绘图
     extent_CN = [88, 124, 22, 38]  # Increase vertical spacing between subplots
     xticks1 = np.arange(extent_CN[0], extent_CN[1] + 1, 10)
@@ -129,7 +138,7 @@ for KType in range(1, 4):
     for ipic in tq.trange(4):
         ax1 = fig.add_subplot(gs[pic_loc[KType-1][ipic]], projection=ccrs.PlateCarree(central_longitude=180 - 70))
         ax1.set_aspect('auto')
-        if ipic==0: ax1.set_title(f"{chr(ord('a') + KType-1)})Temp. pert budget of type{KType}", fontsize=12, loc='left')
+        if ipic==0: ax1.set_title(f"{chr(ord('a') + KType-1)})Temp. pert budget&Surf. Energy of type{KType}", fontsize=12, loc='left')
         ax1.set_extent(extent_CN, crs=ccrs.PlateCarree(central_longitude=0))
         ####
         num_times = [0.1, 10, 10, 10]
@@ -205,8 +214,8 @@ for KType in range(1, 4):
     elif KType == 2:
         reg_map_ = masked(reg_map, r'D:\PyFile\map\self\长江_TP\长江_tp.shp')
     reg = masked(reg_map, r"D:\PyFile\map\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp").sel(
-        type=KType, level=[850, 900]).mean(dim='level')
-    reg_ = reg_map_.sel(type=KType, level=[850, 900]).mean(dim='level')
+        type=KType, level=p_lev)
+    reg_ = reg_map_.sel(type=KType, level=p_lev)
 
     dTdt = np.nanmean(reg_['dTdt']) * 86400 * 31
     adv_X_dTdt = np.nanmean(reg_['adv_T']) * 86400 * 31
@@ -231,67 +240,103 @@ for KType in range(1, 4):
 plt.savefig(f'D:/PyFile/p2/pic/温度收支type.png', dpi=600, bbox_inches='tight')
 plt.show()
 
-# # 创建柱状图
-# fig = plt.figure(figsize=(9, 3))
-#
-# for KType in range(1, 4):
-#     if KType == 3:
-#         reg_map_ = masked(reg_map, r"D:\Code\work\Meteorological\p2\map\WYTR\长江_tp.shp")
-#     elif KType == 1:
-#         reg_map_ = masked(reg_map, r"D:\Code\work\Meteorological\p2\map\EYTR\长江_tp.shp")
-#     elif KType == 2:
-#         reg_map_ = masked(reg_map, r'D:\PyFile\map\self\长江_TP\长江_tp.shp')
-#     reg = masked(reg_map, r"D:\PyFile\map\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp").sel(
-#         type=KType, level=[850, 900]).mean(dim='level')
-#     reg_ = reg_map_.sel(type=KType, level=[850, 900]).mean(dim='level')
-#
-#     adv_X_dTdt = np.nanmean(reg_['adv_T']) * 86400 * 31
-#     ver_X_dTdt = np.nanmean(reg_['ver']) * 86400 * 31
-#     Q_X_dTdt = np.nanmean(reg_['Q']) * 86400 * 31
-#
-#     # 在计算完三个变量之后添加以下代码
-#
-#     # 准备绘图数据
-#     variables = ['Adv', 'Ver', 'Q']
-#     values = [adv_X_dTdt, ver_X_dTdt, Q_X_dTdt]
-#     colors = ['red' if val > 0 else 'blue' for val in values]
-#
-#
-#     # 添加标题和网格
-#     ax = fig.add_subplot(1, 3, KType)
-#     ax.set_title(f'{chr(ord("a") + KType - 1)})Temp. pert budget of type{KType}', fontsize=12,loc='left')
-#     ax.grid(True, linestyle='--', zorder=0, axis='y')
-#
-#     bars = ax.bar(range(3), values, width=0.3, color=colors, edgecolor='black', zorder=2)
-#
-#     # 设置坐标轴标签,字体为 Times New Roman
-#     ax.set_xticks(range(3))
-#     ax.set_xticklabels([r'$-(\mathbf{v} \cdot \nabla T)^{\prime}$',
-#                         r'$(\omega \sigma)^{\prime}$',
-#                         r'${\dot{Q}}^{\prime}$'], fontsize=12, fontname='Times New Roman')
-#
-#     # 设置y轴范围
-#     ymax = 3
-#     ax.set_ylim(-ymax, ymax)
-#
-#     #仅当 KType == 1 时添加y刻度标签
-#     if KType == 1:
-#         ax.set_yticks(np.arange(-3, 4, 1))
-#         ax.set_yticklabels(np.arange(-3, 4, 1), fontsize=12)
-#     else:
-#         ax.set_yticks(np.arange(-3, 4, 1))
-#         ax.set_yticklabels([])
-#         ax.tick_params(axis='y', left=False)
-#
-#     # 添加零线
-#     ax.axhline(0, color='black', lw=1)
-#
-#     # 边框显示为黑色
-#     ax.spines['top'].set_color('black')
-#     ax.spines['right'].set_color('black')
-#     ax.spines['bottom'].set_color('black')
-#     ax.spines['left'].set_color('black')
-#
-# plt.tight_layout()
-# plt.savefig(f'D:/PyFile/p2/pic/温度收支type_bar.png', dpi=600, bbox_inches='tight')
-# plt.show()
+# 辐射收支
+surface_radio = xr.open_dataset(r"D:/PyFile/p2/data/Air_Radio.nc")  # 为地面供能为正，放能为负
+radio = surface_radio
+reg_radio, corr_radio = (
+    np.zeros((3, 4, len(radio.lat), len(radio.lon))),
+    np.zeros((3, 4, len(radio.lat), len(radio.lon))))
+
+for i in tq.trange(len(K_type['type'])):
+    K_series = K_type.sel(type=i + 1)['K'].data
+    if i != 1:
+        K_series = (K_series - np.mean(K_series)) / np.std(K_series)
+        reg_radio[i, 0], corr_radio[i, 0] = regress(K_series, radio['ssr'].data / 86400)  # W/m^2
+        reg_radio[i, 1], corr_radio[i, 1] = regress(K_series, radio['str'].data / 86400)
+        reg_radio[i, 2], corr_radio[i, 2] = regress(K_series, radio['sshf'].data / 86400)
+        reg_radio[i, 3], corr_radio[i, 3] = regress(K_series, radio['slhf'].data / 86400)
+    else:
+        reg_radio[i, 0], corr_radio[i, 0] = regress(K_series[:-1], radio['ssr'].sel(year=slice(1961, 2021)).data / 86400)
+        reg_radio[i, 1], corr_radio[i, 1] = regress(K_series[:-1], radio['str'].sel(year=slice(1961, 2021)).data / 86400)
+        reg_radio[i, 2], corr_radio[i, 2] = regress(K_series[:-1], radio['sshf'].sel(year=slice(1961, 2021)).data / 86400)
+        reg_radio[i, 3], corr_radio[i, 3] = regress(K_series[:-1], radio['slhf'].sel(year=slice(1961, 2021)).data / 86400)
+
+reg_radio = xr.Dataset({'ssr': (['type', 'lat', 'lon'], reg_radio[:, 0]),
+                      'str': (['type', 'lat', 'lon'], reg_radio[:, 1]),
+                      'sshf': (['type', 'lat', 'lon'], reg_radio[:, 2]),
+                      'slhf': (['type', 'lat', 'lon'], reg_radio[:, 3])},
+                     coords={'type': K_type['type'], 'lat': radio.lat, 'lon': radio.lon})
+corr_radio = xr.Dataset({'ssr': (['type', 'lat', 'lon'], corr_radio[:, 0]),
+                       'str': (['type', 'lat', 'lon'], corr_radio[:, 1]),
+                       'sshf': (['type', 'lat', 'lon'], corr_radio[:, 2]),
+                       'slhf': (['type', 'lat', 'lon'], corr_radio[:, 3])},
+                      coords={'type': K_type['type'], 'lat': radio.lat, 'lon': radio.lon})
+
+
+# 创建柱状图
+fig = plt.figure(figsize=(12, 3))
+
+for KType in range(1, 4):
+    if KType == 3:
+        reg_map_ = masked(reg_map, r"D:\Code\work\Meteorological\p2\map\WYTR\长江_tp.shp")
+        reg_radio_ = masked(reg_radio, r"D:\Code\work\Meteorological\p2\map\WYTR\长江_tp.shp")
+    elif KType == 1:
+        reg_map_ = masked(reg_map, r"D:\Code\work\Meteorological\p2\map\EYTR\长江_tp.shp")
+        reg_radio_ = masked(reg_radio, r"D:\Code\work\Meteorological\p2\map\EYTR\长江_tp.shp")
+    elif KType == 2:
+        reg_map_ = masked(reg_map, r'D:\PyFile\map\self\长江_TP\长江_tp.shp')
+        reg_radio_ = masked(reg_radio, r'D:\PyFile\map\self\长江_TP\长江_tp.shp')
+    reg = masked(reg_map, r"D:\PyFile\map\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp").sel(type=KType, level=p_lev)
+    reg_radio_ = masked(reg_radio_, r"D:\PyFile\map\地图边界数据\长江区1：25万界线数据集（2002年）\长江区.shp").sel(type=KType)
+    reg_ = reg_map_.sel(type=KType, level=p_lev)
+
+    adv_X_dTdt = np.nanmean(reg_['adv_T']) * 86400 * 31
+    ver_X_dTdt = np.nanmean(reg_['ver']) * 86400 * 31
+    Q_X_dTdt = np.nanmean(reg_['Q']) * 86400 * 31
+    Radio = np.nanmean(reg_radio_['ssr']) + np.nanmean(reg_radio_['str']) + np.nanmean(reg_radio_['sshf']) + np.nanmean(reg_radio_['slhf'])
+
+    # 准备绘图数据
+    variables = ['Adv', 'Ver', 'Q', 'Surf. E.']
+    values = [adv_X_dTdt, ver_X_dTdt, Q_X_dTdt, Radio]
+    colors = ['red' if val > 0 else 'blue' for val in values]
+
+
+    # 添加标题和网格
+    ax = fig.add_subplot(1, 3, KType)
+    ax.set_title(f'{chr(ord("a") + KType - 1)})Temp. pert budget of type{KType}', fontsize=12,loc='left')
+    ax.grid(True, linestyle='--', zorder=0, axis='y')
+
+    bars = ax.bar(range(4), values, width=0.3, color=colors, edgecolor='black', zorder=2)
+
+    # 设置坐标轴标签,字体为 Times New Roman
+    ax.set_xticks(range(4))
+    ax.set_xticklabels([r'$-(\mathbf{v} \cdot \nabla T)^{\prime}$',
+                        r'$(\omega \sigma)^{\prime}$',
+                        r'${\dot{Q}}^{\prime}$',
+                        r'Surf. E.'], fontsize=12, fontname='Times New Roman')
+
+    # 设置y轴范围
+    ymax = 3
+    ax.set_ylim(-ymax, ymax)
+
+    #仅当 KType == 1 时添加y刻度标签
+    if KType == 1:
+        ax.set_yticks(np.arange(-3, 4, 1))
+        ax.set_yticklabels(np.arange(-3, 4, 1), fontsize=12)
+    else:
+        ax.set_yticks(np.arange(-3, 4, 1))
+        ax.set_yticklabels([])
+        ax.tick_params(axis='y', left=False)
+
+    # 添加零线
+    ax.axhline(0, color='black', lw=1)
+
+    # 边框显示为黑色
+    ax.spines['top'].set_color('black')
+    ax.spines['right'].set_color('black')
+    ax.spines['bottom'].set_color('black')
+    ax.spines['left'].set_color('black')
+
+plt.tight_layout()
+plt.savefig(f'D:/PyFile/p2/pic/温度收支type_bar.png', dpi=600, bbox_inches='tight')
+plt.show()
