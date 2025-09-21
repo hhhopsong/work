8174734506f392213dded6ab30c3a71e6e56260e
Hopsong <1057422494@qq.com>
2025-09-21 16:41:28 +0800

    upupup

--- Meteorological/toolbar/move_detrend.py
+++ Meteorological/toolbar/move_detrend.py
@@ -33,6 +33,9 @@ def mdetrend(series, window_size):
             # 预测当前时间点的趋势值
             current_year = len(window)
             trend_values[i] = lr.predict([[current_year]])[0]
+        elif window_size == 1:
+            window = series[i - window_size:i]
+            trend_values[i] = window
         else:
             trend_values[i] = np.nan
     detrended_series = series - trend_values
